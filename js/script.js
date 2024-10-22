565fbde12ed9860f60e1ca63a120181fb1f534ec
alejandrarobaina <alerobainauni@gmail.com>
2024-10-22 19:48:11 +0200

    Cards en Proceso

--- js/script.js
+++ js/script.js
@@ -34,28 +34,6 @@ $("#menu .fa-bars").click(function(){
             submenu.classList.toggle('show'); // Cambia la clase 'show' para mostrar/ocultar el submenú
         });
   
-
-        
-  // Initialize ChartJS
-  const ctx = $('#myChart');
-  
-  new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-        datasets: [{
-          label: '# Temperatura media en grados celcius',
-          data: [15, 19, 20, 27, 32, 34, 29, 27, 26, 32, 22, 19],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
+      
   
     
