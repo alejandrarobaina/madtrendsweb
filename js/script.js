3513203c2fd4f37a5aa810b252440ee3c856a8ac
alejandrarobaina <alerobainauni@gmail.com>
2024-11-09 17:25:31 +0100

    correciones

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
-// MENU 
-$("#menu .fa-bars").click(function(){
-    console.log("click");
-  
-    $("#menu ul").css("left", "0");
-  });
-  
-  $("#menu li").click(function(){
-    console.log("cerrar");
-    $("#menu ul").css("left", "-100%");
-  });
+
 
 // MENU HAMBURGUESA
-  document.addEventListener("DOMContentLoaded", function () {
-    const navbarToggler = document.querySelector(".navbar-toggler");
-    const navbarCollapse = document.querySelector("#navbarNav");
+$(document).ready(function () {
+    const $navbarToggler = $(".navbar-toggler");
+    const $navbarCollapse = $("#navbarNav");
 
-    navbarToggler.addEventListener("click", function () {
-        // Si el submenú está abierto, se cierra al hacer clic
-        if (navbarCollapse.classList.contains("show")) {
-            navbarCollapse.classList.remove("show");
-        } else {
-            navbarCollapse.classList.add("show");
-        }
+    $navbarToggler.on("click", function () {
+        $navbarCollapse.toggleClass("show");
     });
 });
 
@@ -30,47 +15,41 @@ $("#menu .fa-bars").click(function(){
 // LOGO MADTRENDS 
   $(document).ready(function() {
     const $logo = $('#logo');
-    const originalSrc = 'img/home/madtrendslogoblanco.png'; // Ruta de la imagen original
-    const hoverSrc = 'img/home/madtrendslogonegro.png'; // Ruta de la imagen del menú
-    const buttonHoverSrc = 'img/home/madtrendslogorosa.png'; // Ruta de la imagen del botón
+    const originalSrc = 'img/home/madtrendslogoblanco.png'; 
+    const hoverSrc = 'img/home/madtrendslogonegro.png'; 
+    const buttonHoverSrc = 'img/home/madtrendslogorosa.png'; 
     const $menu = $('.menu');
     const $navbarToggler = $('.navbar-toggler');
     const $submenu = $('#navbarNav');
 
-    // Cambiar la imagen al hacer hover en el contenedor del menú
     $menu.on('mouseenter', function() {
         $logo.attr('src', hoverSrc);
     });
 
-    // Restaurar la imagen original al salir del hover en el menú
     $menu.on('mouseleave', function() {
         if (!$logo.is(':hover')) {
-            $logo.attr('src', originalSrc); // Vuelve a la imagen original si el logo no está en hover
+            $logo.attr('src', originalSrc); 
         }
     });
 
-    // Cambiar la imagen al hacer hover en el logo
     $logo.on('mouseenter', function() {
-        $logo.attr('src', buttonHoverSrc); // Cambia a rosa
+        $logo.attr('src', buttonHoverSrc);
     });
 
-    // Restaurar la imagen al salir del hover en el logo
     $logo.on('mouseleave', function() {
         if ($menu.is(':hover')) {
-            $logo.attr('src', hoverSrc); // Mantiene la imagen negra si el menú sigue en hover
+            $logo.attr('src', hoverSrc); 
         } else {
-            $logo.attr('src', originalSrc); // Vuelve a la imagen original
+            $logo.attr('src', originalSrc);
         }
     });
 
-    // Redirigir al hacer clic en el logo
     $logo.on('click', function() {
-        window.location.href = 'index.html'; // Redirige a index.html
+        window.location.href = 'index.html'; 
     });
 
-    // JavaScript para controlar el comportamiento del menú hamburguesa
     $navbarToggler.on('click', function() {
-        $submenu.toggleClass('show'); // Cambia la clase 'show' para mostrar/ocultar el submenú
+        $submenu.toggleClass('show'); 
     });
 });
 
@@ -102,5 +81,17 @@ $(document).ready(function(){
                 }
             }
         ]
+    });
+});
+
+
+// AOS
+$(document).ready(function(){
+    AOS.init({
+        offset: 90,      
+        delay: 0,        
+        duration: 300, 
+        easing: 'ease-in-out', 
+        once: true       
     });
 });
